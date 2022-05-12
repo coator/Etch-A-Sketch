b52c7071a3a340dcf3c9609e82881a1b38dd0a00
alex w <awaite67@gmail.com>
2022-05-11 22:52:12 -0400

    created script.js and style.css file; begin with mainBody and individual block insert function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const block = document.createElement('div',)
+
+const blockHolder = document.createElement('mainbody')
+
+document.body.appendChild(blockHolder);
+
+const bb = document.getElementsByTagName('mainBody')
+
+bb.backgroundColor = '#666';
+bb.width = '50%';
+bb.height = '50%';
+
+let johnMadden = 20;
+
+
+for (let i = 0; i < johnMadden; i++) {
+    let a = document.createElement('mainbody.block'+i)
+    blockHolder.textContent = i;
+    blockHolder.appendChild(a)
+    let aa = document.getElementsByTagName('mainbody.block'+i)
+    aa.width = '20px';
+    aa.height = '20px';
+    console.log(a)
+}
+
