41b3dd466819c7282cf59816f2807df5b2cc10ae
alex w <awaite67@gmail.com>
2022-05-18 00:21:03 -0400

    added slider bar design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// https://www.youtube.com/watch?v=-5KAN9_CzSA
+// adds stylesheet fom var styles
 function generateStylesheet(){
     let styles = `
 
@@ -19,11 +19,11 @@ function generateStylesheet(){
             margin-top: 0px;
             width: 300px;
             height:400px;
-            border: 2px solid black;
+            border: 10px solid #111111;
             background-color: #252525;
             color: #898989;
             font: 24px Arial, sans-serif;
-            padding: 15px 0px 15px 5px;
+            padding: 15px 15px 15px 5px;
         }
 
         #etchASketchHolder {
@@ -61,73 +61,57 @@ function generateStylesheet(){
             color:#fff;
             transition: background-color 2s, color 2s;
             }
-
-        .slideContainer {
-            width: 100%;
-            }
-
+            
+        /* The slider itself */
         .slider {
-            -webkit-appearance: none; /* overrides default appearance*/
-            appearance: none;
-            width: 100%;
-            height: 25px;
-            background: #333333;
-            outline: none;
-            opacity: 0.7;
-            -webkit-transistion: .2s;
-            transition: opacity .2s;
-            }
-
+        -webkit-appearance: none;  /* Override default CSS styles */
+        appearance: none;
+        width: 101%; /* Full-width */
+        height: 25px; /* Specified height */
+        background: #d3d3d3; /* Grey background */
+        outline: none; /* Remove outline */
+        opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+        -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+        transition: opacity .2s;
+        }
+        
         /* Mouse-over effects */
         .slider:hover {
-            opacity: 1; /* Fully shown on mouse-over */
-            }
+        opacity: 1; /* Fully shown on mouse-over */
+        }
         
         /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
         .slider::-webkit-slider-thumb {
-            -webkit-appearance: none; /* Override default look */
-            appearance: none;
-            width: 25px; /* Set a specific slider handle width */
-            height: 25px; /* Slider handle height */
-            background: #04AA6D; /* Green background */
-            cursor: pointer; /* Cursor on hover */
-            }
+        -webkit-appearance: none; /* Override default look */
+        appearance: none;
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #04AA6D; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+        }
         
         .slider::-moz-range-thumb {
-            width: 25px; /* Set a specific slider handle width */
-            height: 25px; /* Slider handle height */
-            background: #04AA6D; /* Green background */
-            cursor: pointer; /* Cursor on hover */
-            }
-
-
-        }`
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #04AA6D; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+        }
+    }`
 
     let styleSheet = document.createElement("style")
     styleSheet.innerText = styles
     document.head.appendChild(styleSheet)
 }
 
-function generateDivTop(){
-    const topHolder = document.createElement("div")
-    const topHolderLeft = document.createElement("div")
-    const topHolderRight = document.createElement("div")
-    topHolder.setAttribute('id', 'topHolder')
-    topHolderLeft.setAttribute('class', 'boxLeft')
-    topHolderRight.setAttribute('class', 'boxRight')
-    document.body.appendChild(topHolder)
-    topHolderLeft.textContent = 'coator'
-    topHolder.appendChild(topHolderLeft)
-    topHolderRight.textContent = 'etch-a-sketch'
-    topHolder.appendChild(topHolderRight)
-}
-
+// generates the hexidecimal six color string for the random color
 function rndHexGenerator(){
     colorStr = '0123456789ABCDEF';
     let randColor = '#'
     for (let i = 0; i < 6; ++i) randColor = randColor + colorStr[Math.round(Math.random()*15)]
-    return randColor}
+    return randColor
+    }
 
+// creates initial 285 15x19 grid
 function generateStartGrid(){
     function changeColor(obj){
         // math to help determine waht color should be
@@ -142,35 +126,43 @@ function generateStartGrid(){
     }
 }
 
+// sliderbar allows adjustment of the grid size
+function insertSlideBar(divParent){
+    const slideBar =  document.createElement('input');
+    slideBar.setAttribute('class', 'slider');
+    slideBar.setAttribute('id', 'myRange');
+    slideBar.setAttribute('type','range');
+    slideBar.setAttribute('min','1');
+    slideBar.setAttribute('max','100');
+    slideBar.innerHTML = "<a href=''>reset</a> <br />";
+    divParent.appendChild(slideBar)
+    let slider = document.getElementById('myRange');
+    slideBar.addEventListener('click', function() {
+        console.log(slider.value)
+    })
+}
 
-const bodyHolder= document.createElement('div');
-bodyHolder.setAttribute('id', 'bodyHolder');
-document.body.appendChild(bodyHolder)
+// generates bodyHolder (holds sidebar and etchASketch grid) as well as sideBarHolder and etchASketch
+function createLayout(){
+    const bodyHolder= document.createElement('div');
+    bodyHolder.setAttribute('id', 'bodyHolder');
+    document.body.appendChild(bodyHolder)
 
-const sideBarHolder = document.createElement('div');
-sideBarHolder.setAttribute('id', 'sideBarHolder');
-document.body.appendChild(sideBarHolder)
+    const sideBarHolder = document.createElement('div');
+    sideBarHolder.setAttribute('id', 'sideBarHolder');
+    document.body.appendChild(sideBarHolder)
 
-const links = document.createElement('span')
-const squareSlideBar =  document.createElement('squareSlideBar')
-links.innerHTML = '<td><b>cats are not food</b></td>'
-sideBarHolder.appendChild(links)
-var slider = document.getElementById(squareSlideBar);
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+    const links = document.createElement('span')
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-} 
-sideBarHolder.innerHTML = "<a href=''>reset</a> <br /> <input type='range' min='1' max='100' value='50' class='slider' id='squareRange'>"
+    const etchASketchHolder = document.createElement("div");
+    etchASketchHolder.setAttribute('id','etchASketchHolder');
+    document.body.appendChild(etchASketchHolder)
 
-const etchASketchHolder = document.createElement("div");
-etchASketchHolder.setAttribute('id','etchASketchHolder');
-document.body.appendChild(etchASketchHolder)
-
-bodyHolder.appendChild(sideBarHolder)
-bodyHolder.appendChild(etchASketchHolder)
+    bodyHolder.appendChild(sideBarHolder)
+    insertSlideBar(sideBarHolder)
+    bodyHolder.appendChild(etchASketchHolder)
+}
 
 generateStylesheet();
+createLayout()
 generateStartGrid();
