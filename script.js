d458e8636a6ecb7cb992694851806805ae501158
alex w <awaite67@gmail.com>
2022-05-18 21:23:01 -0400

    i broke it

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function generateStylesheet(){
             background-color: #252525;
             color: #898989;
             font: 24px Arial, sans-serif;
-            padding: 15px 15px 15px 5px;
+            padding: 30px 15px 15px 5px;
         }
 
         #etchASketchHolder {
@@ -34,7 +34,7 @@ function generateStylesheet(){
             flex-direction: row;
             flex-wrap: wrap;
             width: 960px;
-            height: 60%;
+            height: 750px;
             padding: 15px;
             font-family: Arial, Helvetica, sans-serif;
             
@@ -42,8 +42,6 @@ function generateStylesheet(){
 
         .block {
             background-color: black;
-            width: 48px;
-            height: 48px;
             border: 1px solid black;
             display: inline-block;
             transition: background-color 2s, color 2s;
@@ -111,46 +109,80 @@ function rndHexGenerator(){
     return randColor
     }
 
-// creates initial 285 15x19 grid
+// creates initial 285 15x19 grid 960x750
 function generateStartGrid(){
-    function changeColor(obj){
-        // math to help determine waht color should be
-        obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)} 
-    const gridLayout = 285;
+    //block.setAttribute('class', 'block');
+    let gridLayout = 285;
+    //block.setAttribute('width', `48px`)
+    //block.setAttribute('height', `48px`)
+    function changeColor(obj){  // math to help determine waht color should be
+        obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)}   
     for (let i = 0; i < gridLayout; i++) {
-        let block = document.createElement("div");
-        block.setAttribute('class', 'block');
+        let block = document.createElement("div");  
         block.addEventListener("mouseover", function() {
-            changeColor(block)})
-        etchASketchHolder.appendChild(block);
-    }
+            changeColor(block)});
+        let values = {class:'block',width:'48px',height:'48px'} 
+        for (const key in values){ 
+            block.setAttribute(`${key}`,`${values[key]}`)
+        etchASketchHolder.appendChild(block)
+        console.log(gridLayout)
+    }}
 }
 
-// sliderbar allows adjustment of the grid size
-function insertSlideBar(divParent){
-    const slideBar =  document.createElement('input');
-    slideBar.setAttribute('class', 'slider');
-    slideBar.setAttribute('id', 'myRange');
-    slideBar.setAttribute('type','range');
-    slideBar.setAttribute('min','1');
-    slideBar.setAttribute('max','100');
 
+function generateStartGrid2(newValue){
+    function changeColor(obj){  // math to help determine waht color should be
+            obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)} 
+    let block = document.createElement("div");
+    block.setAttribute('class', 'block');
+    let gridLayout = 285;
+    let selectionBlockSizes = [48,40,36,32,30,27,24,20,16,12]
+    console.log(selectionBlockSizes[newValue])
+    if (newValue < 0) {
+        block.setAttribute('width', `48px`)
+        block.setAttribute('height', `48px`)
+    } else {
+        newBlockSize =selectionBlockSizes[newValue]*selectionBlockSizes[newValue]
+        gridLayout = (960*750) / (selectionBlockSizes[newValue]*selectionBlockSizes[newValue]) 
+        block.setAttribute('width', `${selectionBlockSizes[newValue]}`)
+        block.setAttribute('height', `${selectionBlockSizes[newValue]}`)
+    block.addEventListener("mouseover", function() {
+        changeColor(block)});
+    for (let i = 0; i < gridLayout; i++) {
+        etchASketchHolder.appendChild(block)
+        console.log(gridLayout)
+    }
+}}
+
+
+function insertSlideBar(divParent){ // sliderbar allows adjustment of the grid size
+    const slideBar =  document.createElement('input');
+    let values= {class:'slider',
+                id:'myRange',
+                type:'range',
+                min:'1',
+                max:'10'};     
+    for (const key in values){
+        slideBar.setAttribute(`${key}`,`${values[key]}`)
+    }
     const slideBarReset = document.createElement('a')
-    const values = {href:'' , id:'slideBarReset'}
+    values = {href:'' , id:'slideBarReset'}
     for (const key in values){
         slideBarReset.setAttribute(`${key}`,`${values[key]}`)
     }
+    slideBarReset.innerHTML='reset'
     divParent.appendChild(slideBar)
     divParent.appendChild(slideBarReset)
 
     let slider = document.getElementById('myRange');
+
     slideBar.addEventListener('click', function() {
-        console.log(slider.value)
+        generateStartGrid2(slider.value)
     })
 }
 
-// generates bodyHolder (holds sidebar and etchASketch grid) as well as sideBarHolder and etchASketch
-function createLayout(){
+
+function createLayout(){ // generates bodyHolder (holds sidebar and etchASketch grid) as well as sideBarHolder and etchASketch
     const bodyHolder= document.createElement('div');
     bodyHolder.setAttribute('id', 'bodyHolder');
     document.body.appendChild(bodyHolder)
@@ -171,5 +203,5 @@ function createLayout(){
 }
 
 generateStylesheet();
-createLayout()
+createLayout();
 generateStartGrid();
