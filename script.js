2443429680ca8218309ff78acd4892d655666cd2
alex w <awaite67@gmail.com>
2022-05-18 22:39:25 -0400

    - created separate function to fix issues regarding slider bar changing values - need to work on resizing issues when onhover is activated

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ function generateStylesheet(){
             border: 1px solid black;
             display: inline-block;
             transition: background-color 2s, color 2s;
+            width:48px;
+            height:48px;
             }    
 
         .invisible {
@@ -109,50 +111,43 @@ function rndHexGenerator(){
     return randColor
     }
 
+function changeColor(obj){  // math to help determine waht color should be
+    obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)}   
+
+
 // creates initial 285 15x19 grid 960x750
 function generateStartGrid(){
-    //block.setAttribute('class', 'block');
     let gridLayout = 285;
-    //block.setAttribute('width', `48px`)
-    //block.setAttribute('height', `48px`)
-    function changeColor(obj){  // math to help determine waht color should be
-        obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)}   
     for (let i = 0; i < gridLayout; i++) {
         let block = document.createElement("div");  
-        block.addEventListener("mouseover", function() {
-            changeColor(block)});
-        let values = {class:'block',width:'48px',height:'48px'} 
-        for (const key in values){ 
-            block.setAttribute(`${key}`,`${values[key]}`)
-        etchASketchHolder.appendChild(block)
-        console.log(gridLayout)
+        block.addEventListener("mouseover", function() {changeColor(block)});
+        block.setAttribute('class','block');
+        etchASketchHolder.appendChild(block);
     }}
-}
 
+ //let values = {class:'block',width:'48px',height:'48px'};
+ //       for (const key in values){ 
+ //           block.setAttribute(`${key}`,`${values[key]}`)};
 
-function generateStartGrid2(newValue){
-    function changeColor(obj){  // math to help determine waht color should be
-            obj.setAttribute('style', `background-color: ${rndHexGenerator()}`)} 
-    let block = document.createElement("div");
-    block.setAttribute('class', 'block');
+function generateGrid(newValue){
     let gridLayout = 285;
-    let selectionBlockSizes = [48,40,36,32,30,27,24,20,16,12]
-    console.log(selectionBlockSizes[newValue])
-    if (newValue < 0) {
-        block.setAttribute('width', `48px`)
-        block.setAttribute('height', `48px`)
-    } else {
-        newBlockSize =selectionBlockSizes[newValue]*selectionBlockSizes[newValue]
-        gridLayout = (960*750) / (selectionBlockSizes[newValue]*selectionBlockSizes[newValue]) 
-        block.setAttribute('width', `${selectionBlockSizes[newValue]}`)
-        block.setAttribute('height', `${selectionBlockSizes[newValue]}`)
-    block.addEventListener("mouseover", function() {
-        changeColor(block)});
+    let sizeObj = [48,40,36,32,30,27,24,20,16,12]
+    let sizeObjStr = ['48px','40px','36px','32px','30px','27px','24px','20px','16px','12px']
+    newBlockSize =sizeObj[newValue]*sizeObj[newValue]
+    gridLayout = (960*750) / (sizeObj[newValue]*sizeObj[newValue])
+    const parent = document.getElementById('etchASketchHolder') 
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);}
+    console.log(gridLayout)
     for (let i = 0; i < gridLayout; i++) {
-        etchASketchHolder.appendChild(block)
-        console.log(gridLayout)
-    }
-}}
+        let block = document.createElement("div");  
+        block.addEventListener("mouseover", function() {changeColor(block)});
+        block.setAttribute('class','block');
+        console.log(sizeObjStr[newValue])
+        block.style.width=`${sizeObjStr[newValue]}`
+        block.style.height=`${sizeObjStr[newValue]}`
+        etchASketchHolder.appendChild(block); }
+}
 
 
 function insertSlideBar(divParent){ // sliderbar allows adjustment of the grid size
@@ -163,13 +158,11 @@ function insertSlideBar(divParent){ // sliderbar allows adjustment of the grid s
                 min:'1',
                 max:'10'};     
     for (const key in values){
-        slideBar.setAttribute(`${key}`,`${values[key]}`)
-    }
+        slideBar.setAttribute(`${key}`,`${values[key]}`)}
     const slideBarReset = document.createElement('a')
     values = {href:'' , id:'slideBarReset'}
     for (const key in values){
-        slideBarReset.setAttribute(`${key}`,`${values[key]}`)
-    }
+        slideBarReset.setAttribute(`${key}`,`${values[key]}`)}
     slideBarReset.innerHTML='reset'
     divParent.appendChild(slideBar)
     divParent.appendChild(slideBarReset)
@@ -177,7 +170,7 @@ function insertSlideBar(divParent){ // sliderbar allows adjustment of the grid s
     let slider = document.getElementById('myRange');
 
     slideBar.addEventListener('click', function() {
-        generateStartGrid2(slider.value)
+        generateGrid(slider.value)
     })
 }
 
